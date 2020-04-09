d9b4bfbb27d2a8754fe975a6afecfc6180f8e111
ETenal <etenal@mail.etenal.me>
2020-04-09 13:36:27 -0700

    still fix bugs in deploy.py

--- deploy.py
+++ deploy.py
@@ -1,5 +1,6 @@
 import re
 import os
+import requests
 
 from subprocess import call
 
@@ -34,9 +35,9 @@ class Deployer:
     def deploy(self, cases):
         for hash in cases:
             case = cases[hash]
-            syzkaller_path = self.__run_delopy_script(hash, case)
+            #syzkaller_path = self.__run_delopy_script(hash, case)
+            syzkaller_path = ''
             self.__write_config(syzkaller_path, case["syz_repro"])
-            #self.__write_testcase(case["syz_repro"])
 
     def clone_linux(self):
         self.__run_linux_clone_script()
@@ -53,13 +54,18 @@ class Deployer:
         print("run: scripts/deploy.sh {} {} {} {}".format(self.linux_path, hash, commit, syzkaller, config, testcase))
         return call(["scripts/deploy.sh", self.linux_path, hash, commit, syzkaller, config, testcase], shell=True)
 
-    def __write_config(self, syzkaller_path, testcase):
-        syscalls = self.__extract_syscalls(testcase)
+    def __write_config(self, syzkaller_path, testcase_url):
+        req = requests.request(method='GET', url=testcase_url)
+        testcase = req.content
+        syscalls = self.__extract_syscalls(testcase.decode("utf-8"))
         if syscalls == []:
             print("No syscalls found in testcase: {}".format(testcase))
             return -1
+        print(syscalls)
         last_syscall = syscalls[len(syscalls)-1]
         dependent_syscalls = self.__extract_dependent_syscalls(last_syscall, syzkaller_path)
+        if len(dependent_syscalls) < 1:
+            print("Cannot find dependent syscalls for {}.\nTry to continue without them".format(last_syscall))
         syscalls.extend(dependent_syscalls)
         enable_syscalls = "\"" + "\",\n\t\"".join(syscalls)[:-4]
         syz_config_template.format(enable_syscalls)
@@ -67,13 +73,14 @@ class Deployer:
 
     def __extract_syscalls(self, testcase):
         res = []
-        for line in testcase:
-            if line[0] == '#':
+        text = testcase.split('\n')
+        for line in text:
+            if len(line)==0 or line[0] == '#':
                 continue
             m = re.search('(\w+(\$\w+)?)\(', line)
             if m == None or len(m.groups()) == 0:
                 print("Failed to extract syscall from {}".format(line))
-                return -1
+                return res
             syscall = m.groups()[0]
             res.append(syscall)
         return res
@@ -81,6 +88,9 @@ class Deployer:
     def __extract_dependent_syscalls(self, last_syscall, syzkaller_path, search_path="sys/linux", extension=".txt"):
         res = []
         dir = os.path.join(syzkaller_path, search_path)
+        if not os.path.isdir(dir):
+            print("{} do not exist".format(dir))
+            return res
         for file in os.listdir(dir):
             if file.endswith(extension):
                 find_it = False
