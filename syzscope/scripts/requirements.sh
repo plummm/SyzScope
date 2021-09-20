e612d19403acbf2c7a905f47bb94e7a5e5a5dd6b
plummm <etenalcxz@gmail.com>
2021-09-20 11:43:05 -0700

    check if sudo doesn't exist, return 1

--- syzscope/scripts/requirements.sh
+++ syzscope/scripts/requirements.sh
@@ -3,9 +3,8 @@
 #
 # Usage ./requirements.sh
 
-sudo || echo "Install sudo before run SyzScope" && exit 1
 if [ ! -f "$(pwd)/tools/.stamp/ENV_SETUP" ]; then
-  sudo apt-get update
+  sudo apt-get update || exit 1
   sudo apt-get -y install gdb curl git wget qemu-system-x86 debootstrap flex bison libssl-dev libelf-dev locales cmake libxml2-dev libz3-dev bc
 fi
 
